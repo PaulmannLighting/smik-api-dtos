e22c6e96add1e36d67302273833334f817e9d955
Richard Neumann <neumann@paulmann.de>
2024-08-27 14:35:26 +0200

    Implement getters

--- src/areas/action.rs
+++ src/areas/action.rs
@@ -8,3 +8,35 @@ pub struct Action {
     color: Rgb,
     color_temperature: u16,
 }
+
+impl Action {
+    #[must_use]
+    pub const fn new(on: bool, brightness: u8, color: Rgb, color_temperature: u16) -> Self {
+        Self {
+            on,
+            brightness,
+            color,
+            color_temperature,
+        }
+    }
+
+    #[must_use]
+    pub const fn on(&self) -> bool {
+        self.on
+    }
+
+    #[must_use]
+    pub const fn brightness(&self) -> u8 {
+        self.brightness
+    }
+
+    #[must_use]
+    pub const fn color(&self) -> &Rgb {
+        &self.color
+    }
+
+    #[must_use]
+    pub const fn color_temperature(&self) -> u16 {
+        self.color_temperature
+    }
+}
