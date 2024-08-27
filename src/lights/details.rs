233dfc6ee11d2a9ebae1163edb3699e4884bc82b
Richard Neumann <neumann@paulmann.de>
2024-08-27 14:37:35 +0200

    Implement getters

--- src/lights/details.rs
+++ src/lights/details.rs
@@ -21,7 +21,52 @@ pub struct Light {
 
 impl Light {
     #[must_use]
+    pub const fn id(&self) -> u32 {
+        self.id
+    }
+
+    #[must_use]
+    pub const fn is_deleted(&self) -> bool {
+        self.is_deleted
+    }
+
+    #[must_use]
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    #[must_use]
+    pub const fn icon(&self) -> Icon {
+        self.icon
+    }
+
+    #[must_use]
     pub const fn state(&self) -> &State {
         &self.state
+    }
+
+    #[must_use]
+    pub const fn capabilities(&self) -> &Capabilities {
+        &self.capabilities
+    }
+
+    #[must_use]
+    pub const fn is_new(&self) -> bool {
+        self.is_new
+    }
+
+    #[must_use]
+    pub fn company(&self) -> &str {
+        &self.company
+    }
+
+    #[must_use]
+    pub fn firmware_version(&self) -> &str {
+        &self.firmware_version
+    }
+
+    #[must_use]
+    pub fn serial_number(&self) -> &str {
+        &self.serial_number
     }
 }
