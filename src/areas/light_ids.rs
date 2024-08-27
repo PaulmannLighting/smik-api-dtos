9833d7544895298341b4697820e0165658967893
Richard Neumann <neumann@paulmann.de>
2024-08-27 14:33:13 +0200

    Implement getter

--- src/areas/light_ids.rs
+++ src/areas/light_ids.rs
@@ -11,4 +11,14 @@ impl LightIds {
     pub const fn new(area_id: u32, light_ids: Vec<u32>) -> Self {
         Self { area_id, light_ids }
     }
+
+    #[must_use]
+    pub const fn area_id(&self) -> u32 {
+        self.area_id
+    }
+
+    #[must_use]
+    pub fn light_ids(&self) -> &[u32] {
+        &self.light_ids
+    }
 }
