c3cbb8fd540d6b54c9b2fc8dbdc0d59731e2901d
Richard Neumann <neumann@paulmann.de>
2024-08-27 14:39:53 +0200

    Implement conversion traits

--- src/lights/reachable.rs
+++ src/lights/reachable.rs
@@ -2,6 +2,35 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
 pub struct Reachable {
-    #[serde(skip_serializing_if = "Option::is_none")]
-    reachable: Option<bool>,
+    #[serde(rename = "reachable", skip_serializing_if = "Option::is_none")]
+    is_reachable: Option<bool>,
+}
+
+impl Reachable {
+    #[must_use]
+    pub const fn reachable(&self) -> Option<bool> {
+        self.is_reachable
+    }
+}
+
+impl From<Option<bool>> for Reachable {
+    fn from(reachable: Option<bool>) -> Self {
+        Self {
+            is_reachable: reachable,
+        }
+    }
+}
+
+impl From<bool> for Reachable {
+    fn from(reachable: bool) -> Self {
+        Self {
+            is_reachable: Some(reachable),
+        }
+    }
+}
+
+impl From<Reachable> for Option<bool> {
+    fn from(reachable: Reachable) -> Self {
+        reachable.is_reachable
+    }
 }
