d2511fc4aa0efa3c066deb00f6d8b74f938628e5
Richard Neumann <neumann@paulmann.de>
2024-08-27 10:12:37 +0200

    Add LightIds

---
+++ src/areas/light_ids.rs
@@ -0,0 +1,14 @@
+use serde::Serialize;
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize)]
+pub struct LightIds {
+    area_id: u32,
+    light_ids: Vec<u32>,
+}
+
+impl LightIds {
+    #[must_use]
+    pub const fn new(area_id: u32, light_ids: Vec<u32>) -> Self {
+        Self { area_id, light_ids }
+    }
+}
